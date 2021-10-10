b68fcf5c5d3a96ed993c4359a0581bcbf3d46106
Alexey Burlak <burlakop@gmail.com>
2021-10-10 17:24:29 +0400

    debug2.1

--- src/main/java/ru/nshi/learn/work2/Task5.java
+++ src/main/java/ru/nshi/learn/work2/Task5.java
@@ -6,23 +6,26 @@ public class Task5 {
     Task5 value5=new Task5();
     int[] numbers=value.newArray(10);
     numbers=value.randomArray(numbers);
-    value5.arfArray(numbers);
+    System.out.print("Исходный массив: ");
+    value.printArray(numbers);
+    System.out.print("\n"+value5.arfArray(numbers));
  }
 
 
 
- public void arfArray(int[] array){
+ public int arfArray(int[] array){
     Task4 value4=new Task4();
     int imax=value4.imaxArray(array), imin=value4.iminArray(array), max=10, min=100, sum=0;
     if(imax>imin){
         for(int i=imin+1; i<imax; i++){
             sum+=array[i];}
-        System.out.print(sum/(imax-imin-1));
+        return sum/(imax-imin-1);
         }
     if(imin>imax){
         for(int i=imax+1; i<imin; i++){
             sum+=array[i];}
-        System.out.print(sum/(imin-imax-1));
+        return sum/(imin-imax-1);
         }
+    return 0;
 }
 }
