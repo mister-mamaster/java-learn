34bfbcc56b206d8ecab11f0af9d4034fac395b51
Alexey Burlak <burlakop@gmail.com>
2022-04-18 14:40:38 +0400

    OOP-colors

--- main_module/src/main/java/ru/nshi/learn/work6/Figure.java
+++ main_module/src/main/java/ru/nshi/learn/work6/Figure.java
@@ -1,50 +1,22 @@
 package ru.nshi.learn.work6;
 
 public class Figure {
-    protected int[] color = new int[3];
+    protected Colour colour = new Colour();
     protected double area;
-    boolean clrRGB = false, clrHSB = false;
 
     public Figure(){
             this.area = -1;
-    }
-
-    public void setColorRGB(int red, int green, int blue){
-        if((red >= 0 && red <= 255) & (green >= 0 && green <= 255) & (blue >= 0 && blue <= 255)) {
-            this.color[0] = red;
-            this.color[1] = green;
-            this.color[2] = blue;
-            clrRGB = true;
-            clrHSB = false;
-        }
-        else{
-            throw new IllegalArgumentException();
-        }
-    }
-
-    public void setColorHSB(int hue, int saturation, int brightness){
-        if((hue >= 0 && hue <= 360) & (saturation >= 0 && saturation <= 100) & (brightness >= 0 && brightness <= 100)) {
-            this.color[0] = hue;
-            this.color[1] = saturation;
-            this.color[2] = brightness;
-            clrRGB = false;
-            clrHSB = true;
-        }
-        else{
-            throw new IllegalArgumentException();
-        }
     }
 
     public double getArea(){
         return this.area;
     }
 
-    public int[] getcolor() {
-        return color;
+    public Colour getcolour() {
+        return colour;
     }
 
     public String info(){
-        return "Объект типа " + this.getClass().getSimpleName() + "\nЦвет - " + this.color[0] + "/" + this.color[1] + "/" + this.color[2]
-        + "\nПлощадь фигуры - " + this.area + "\n";
+        return "Объект типа " + this.getClass().getSimpleName() + "\nЦвет - " + this.colour.getColors() + "\nПлощадь фигуры - " + this.area + "\n";
     }
 }
--- main_module/src/test/java/ru/nshi/learn/work6/CircleTest.java
+++ main_module/src/test/java/ru/nshi/learn/work6/CircleTest.java
@@ -11,7 +11,7 @@ public class CircleTest {
     void circleTest(){
         Circle circle = new Circle(4);
         double area = circle.getArea();
-        circle.setColorHSB(1,2,3);
+        circle.colour.setColourHSB(1,2,3);
         System.out.print(circle.info());
         assertEquals(50.24, area);
     }
@@ -20,22 +20,22 @@ public class CircleTest {
     void circleExceptionTest(){
         assertThrows(IllegalArgumentException.class, () -> {
             Circle circle = new Circle(4);
-            circle.setColorRGB(100, -2, 200);
+            circle.colour.setColourRGB(100, -2, 200);
         });
         assertThrows(IllegalArgumentException.class, () -> {
             Circle circle = new Circle(4);
-            circle.setColorRGB(300, 100, 100);
+            circle.colour.setColourRGB(300, 100, 100);
         });
         assertThrows(IllegalArgumentException.class, () -> {
             Circle circle = new Circle(-500);
         });
         assertThrows(IllegalArgumentException.class, () -> {
             Circle circle = new Circle(1);
-            circle.setColorHSB(-1, 10, 10);
+            circle.colour.setColourHSB(-1, 10, 10);
         });
         assertThrows(IllegalArgumentException.class, () -> {
             Circle circle = new Circle(1);
-            circle.setColorHSB(1, 101, 101);
+            circle.colour.setColourHSB(1, 101, 101);
         });
     }
 }
