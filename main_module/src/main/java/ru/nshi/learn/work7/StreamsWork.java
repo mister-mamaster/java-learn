9a940982263274a9d5f3072a9ab2404dcad29371
Alexey Burlak <burlakop@gmail.com>
2022-05-11 21:29:29 +0400

    fix 3

--- main_module/src/main/java/ru/nshi/learn/work7/StreamsWork.java
+++ main_module/src/main/java/ru/nshi/learn/work7/StreamsWork.java
@@ -131,13 +131,5 @@ public class StreamsWork {
         return new BufferedReader(new InputStreamReader(connection.getInputStream()));
     }
 
-    static class CarMakerCompare implements Comparator<CarMaker>{
 
-        public int compare(CarMaker cm1, CarMaker cm2){
-
-            return cm1.getName().toUpperCase().compareTo(cm2.getName().toUpperCase());
-
-        }
-
-    }
 }
